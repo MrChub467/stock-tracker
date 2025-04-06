0d9fa5a655ae9aa827af88472191ea3dd2fe2102
MrChub467 <chubster467@gmail.com>
2025-04-06 12:42:12 -0500

    finish up

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import $ from "jquery";
 import anychart from 'anychart';
 
 
-const API_KEY1 = "cvondihr01qihjtpppg0cvondihr01qihjtpppgg";
-const API_KEY2 = "l6Cp8V7e6oJpnThQEB6eN64cE0zq6rWhvnRg7daf";
+const API_KEY1 = process.env.API_KEY1;
+const API_KEY2 = process.env.API_KEY2;
 $(".content").hide();
-
+loadHomePage();
 
 
 $( function() {
+  $(".home-btn").on("click", loadHomePage);
   $(document).on("click", closeAllLists);
   $("#search-bar").on("submit", (e) => {
     e.preventDefault();
@@ -23,16 +24,16 @@ $( function() {
         data: { q: $("#ticker-search").val(), exchange: "US", token: API_KEY1 },
         dataType: "json"
       })
-      .done(function(data) {
-        console.log(data)
-        console.log(data.result[0].symbol)
-        autocomplete(data)
+      .done(function(data, response) {
+        if (response === "429" || response === 429) {
+          $(".details").text("Error: Data limit exceeded. Please try again later");
+        } else {
+          autocomplete(data)
+        }
       })
       .fail(function() {
         console.log("DIDN'T WORK");
       });
-      // autocomplete($("#ticker-search"), ["IBM", "APPL", "NVIDIA"])
-
     }
   })
 });
@@ -54,90 +55,103 @@ function autocomplete(data) {
     }
   } 
 }
-
-  /*execute a function presses a key on the keyboard:*/
-  // inp.on("keydown", function(e) {
-  //     var x = document.getElementById(this.id + "autocomplete-list");
-  //     if (x) x = x.getElementsByTagName("div");
-  //     if (e.keyCode == 40) {
-  //       /*If the arrow DOWN key is pressed,
-  //       increase the currentFocus variable:*/
-  //       currentFocus++;
-  //       /*and and make the current item more visible:*/
-  //       addActive(x);
-  //     } else if (e.keyCode == 38) { //up
-  //       /*If the arrow UP key is pressed,
-  //       decrease the currentFocus variable:*/
-  //       currentFocus--;
-  //       /*and and make the current item more visible:*/
-  //       addActive(x);
-  //     } else if (e.keyCode == 13) {
-  //       /*If the ENTER key is pressed, prevent the form from being submitted,*/
-  //       e.preventDefault();
-  //       if (currentFocus > -1) {
-  //         /*and simulate a click on the "active" item:*/
-  //         if (x) x[currentFocus].click();
-  //       }
-  //     }
-  // });
-  // function addActive(x) {
-  //   /*a function to classify an item as "active":*/
-  //   if (!x) return false;
-  //   /*start by removing the "active" class on all items:*/
-  //   removeActive(x);
-  //   if (currentFocus >= x.length) currentFocus = 0;
-  //   if (currentFocus < 0) currentFocus = (x.length - 1);
-  //   /*add class "autocomplete-active":*/
-  //   x[currentFocus].addClass("autocomplete-active");
-  // }
-  // function removeActive(x) {
-  //   /*a function to remove the "active" class from all autocomplete items:*/
-  //   for (var i = 0; i < x.length; i++) {
-  //     x[i].classList.remove("autocomplete-active");
-  //   }
-  // }
-
 // Close autocomplete list
 function closeAllLists() {
   $("#autocomplete-list").remove();
 }
-/*execute a function when someone clicks in the document:*/
 
-function displayStock(stock) {
-  stock = stock.toUpperCase();
+function loadHomePage() {
+  $(".content").slideUp(1000).delay(1000);
+  setTimeout(function() {
+    clearContent();
+    getMarketStatus();
+    getMarketNews();
+  }, 1000)
+  $(".content").slideDown(1000);
+}
+function getMarketStatus() {
   $.ajax({
-    url: "https://finnhub.io/api/v1/quote?",
+    url: "https://finnhub.io/api/v1/stock/market-status?",
     method: "GET",
-    data: { symbol: stock, token: API_KEY1 },
+    data: { exchange: "US", token: API_KEY1 },
     dataType: "json"
   })
-  .done(function(data) {
-    $(".content").slideUp("slow").delay(800);
-    setTimeout(function() {
-      clearContent();
-      getGraphData(stock);
-      getCompanyProfile(stock);
-      getCompanyNews(stock);
-    }, 500)
-    $(".content").slideDown("slow");
+  .done(function(data, response) {
+    if (response === "429" || response === 429) {
+      $(".details").text("Error: Data limit exceeded. Please try again later");
+    } else {
+      displayMarketStatus(data);
+    }
   })
   .fail(function() {
-    console.log("Didn't get stock details");
+    console.log("Didn't get market status");
   });
 }
-
-function clearContent() {
-  $(".company-header").empty();
-  $(".details").empty();
-  $("#graph").empty();
-  $(".news").empty();
+function displayMarketStatus(data) {
+  const status = $("<div>").addClass("status");
+  const exc = $("<div>").text("Exchange: " + data.exchange);
+  if (data.isOpen === true) {
+    var open = $("<div>").text("Open Status: Open");
+  } else {
+    var open = $("<div>").text("Open Status: Closed");
+  }
+  if (data.session) {
+    var ses = $("<div>").text("Current session: " + data.session);
+  } else {
+    var ses = $("<div>").text("Current session: None");
+  }
+  const zone = $("<div>").text("Timezone: " + (data.timezone).replace("_", " "));
+  status.append(exc, open, ses, zone);
+  $(".content").append(status);
+}
+function getMarketNews() {
+  $.ajax({
+    url: "https://finnhub.io/api/v1/news?",
+    method: "GET",
+    data: { category: "general", token: API_KEY1 },
+    dataType: "json"
+  })
+  .done(function(data, response) {
+    if (response === "429" || response === 429) {
+      $(".details").text("Error: Data limit exceeded. Please try again later");
+    } else {
+      displayMarketNews(data);
+    }
+  })
+  .fail(function() {
+    console.log("Didn't get market news");
+  });
+}
+function displayMarketNews(data) {
+  displayNews(data, "Market");
 }
 
+// Calls functions to get the company data and chart a graph
+function displayStock(stock) {
+  stock = stock.toUpperCase();
+  
+  $(".content").slideUp(1000).delay(1000);
+  setTimeout(function() {
+    clearContent();
+    getGraphData(stock);
+    getCompanyProfile(stock);
+    getCompanyNews(stock);
+  }, 1000)
+  $(".content").slideDown(1000);
+    
+}
+
+// Clears the contents so a new company can be displayed
+function clearContent() {
+  $(".content").empty();
+}
+
+// Gets historical data to build a graph then passes that data to another function that graphs it
 function getGraphData(stock) {
   $.ajax({
-    url: "https://api.stockdata.org/v1/data/eod?",
+    url: "https://api.stockdata.org/v1/data/intraday?",
     method: "GET",
-    data: { symbols: stock, api_token: API_KEY2 },
+    data: { symbols: stock, interval: "hour", api_token: API_KEY2 },
     dataType: "json"
   })
   .done(function(data) {
@@ -147,41 +161,42 @@ function getGraphData(stock) {
     console.log("Didn't get graph data");
   });
 }
-
+// Creates a graph from historical data
 function createGraph(rawData) {
-  const chartData = rawData.data.map(item => [
-    new Date(item.date), item.open, item.high, item.low, item.close
+  const chartData = rawData.data.map((item) => [
+    new Date(item.date), item.data.open, item.data.high, item.data.low, item.data.close
   ]);
   anychart.onDocumentReady(function () {
     // Create a stock chart
     const chart = anychart.stock();
-  
+    
     const series = chart.plot(0).line(chartData);
     series.stroke({
       color: "#8c2fff",
       thickness: 2
     })
-    // Create a candlestick series
+    // Create a Price Indicator series
     const indicator = chart.plot(0).priceIndicator({value: "last-visible"});
     
     series.name("Stock Price");
-
+    // Coloring 
     let background = chart.background();
     background.cornerType("round");
     background.corners(20);
     chart.scroller()
-      .fill("#363C5D")       // background fill of the scroller
+      .fill("#363C5D")
       .selectedFill("#cac7ff");
     chart.background().fill("#FFFFFF20");
     // Set chart title and container
     chart.title("Stock Price Chart");
+    $("<div>").attr("id", "graph").insertAfter($(".details"));
     chart.container("graph");
 
     // Draw the chart
     chart.draw();
   });
 }
-
+// Gets the company profile details then calls a function that displays them
 function getCompanyProfile(stock) {
   $.ajax({
     url: "https://finnhub.io/api/v1/stock/profile2?",
@@ -189,14 +204,18 @@ function getCompanyProfile(stock) {
     data: { symbol: stock, token: API_KEY1 },
     dataType: "json"
   })
-  .done(function(data) {
-    if (Object.keys(data).length !== 0) displayCompanyProfile(data);
+  .done(function(data, response) {
+    if (response === "429" || response === 429) {
+      $(".details").text("Error: Data limit exceeded. Please try again later");
+    } else {
+      if (Object.keys(data).length !== 0) displayCompanyProfile(data);
+    }
   })
   .fail(function() {
     console.log("Didn't get company profile");
   });
 }
-
+// Displays company profile information
 function displayCompanyProfile(data) {
   const companyName = $("<h1>").text(data.name).addClass("company-name");
   const companyCountry = $("<div>").text("Country: " + data.country);
@@ -205,11 +224,10 @@ function displayCompanyProfile(data) {
   const companyIPO = $("<div>").text("Initial Public Offering: " + data.ipo);
   const companyWebsite = $("<a>").text("Website").attr("href", data.weburl);
   const companyLogo = $("<img>").attr("src", data.logo);
-  $(".company-header").append(companyName, companyLogo);
-  $(".details").append(companyCountry, companyExchange, companyIndustry, companyIPO, companyWebsite);
-  
+  $(".content").append($("<header>").addClass("company-header").append(companyName, companyLogo));
+  $(".content").append($("<div>").addClass("details").append(companyCountry, companyExchange, companyIndustry, companyIPO, companyWebsite));
 }
-
+// Gets company news from one week ago till now
 function getCompanyNews(stock) {
   $.ajax({
     url: "https://finnhub.io/api/v1/company-news?",
@@ -217,19 +235,22 @@ function getCompanyNews(stock) {
     data: { symbol: stock, from: getDate(true), to: getDate(), token: API_KEY1 },
     dataType: "json"
   })
-  .done(function(data) {
-    displayCompanyNews(data);
+  .done(function(data, response) {
+    if (response === "429" || response === 429) {
+      $(".details").text("Error: Data limit exceeded. Please try again later");
+    } else {
+      displayNews(data, "Company");
+    }
   })
   .fail(function() {
     console.log("Didn't get company news");
   });
 }
-
-function displayCompanyNews(data) {
-  console.log(data)
+// Displays at most 10 news articles about the company
+function displayNews(data, type) {
   if (data.length > 0) {
-    const newsHeader = $("<h2>").text("Company News");
-    const newsSection = $(".news");
+    const newsHeader = $("<h2>").text(`${type} News`);
+    const newsSection = $("<div>").addClass("news");
     newsSection.append(newsHeader);
     for (let i = 0; i < 10; ++i) {
       const newsHeadline = $("<div>").text(data[i].headline).addClass("headline");
@@ -237,14 +258,16 @@ function displayCompanyNews(data) {
       const newsTime = $("<div>").text(new Date(data[i].datetime * 1000));
       const newsSummary = $("<div>").text(data[i].summary).addClass("summary");
       const newsLink = $("<a>").text("Link to article").attr("href", data[i].url);
+      const newsContainer = $("<div>").addClass("articleContainer")
   
-      newsSection.append($("<div>").append(newsHeadline, newsImage).addClass("article-header"));
-      newsSection.append(newsTime, newsSummary, newsLink);
+      newsContainer.append($("<div>").append(newsHeadline, newsImage).addClass("article-header"), newsTime, newsSummary, newsLink)
+      newsSection.append(newsContainer);
       if (data.length === i + 1) break;
     }
+    $(".content").append(newsSection);
   }
 }
-
+// Get today's and the date from one week ago
 function getDate(past=false) {
   const today = new Date();
   const date = past ? new Date(today.setDate(today.getDate() - 7)) : today;
